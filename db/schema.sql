92d45dcb93fae3266c5ee6f901c47599f01d156a
Ankush <ankushchalla@gmail.com>
2021-02-11 14:32:45 -0800

    db stuff.

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burger) VALUES
+("Double-Double"), ("Bacon Cheesburger"), ("Blackbean");
+
+SELECT * FROM burgers;
+
