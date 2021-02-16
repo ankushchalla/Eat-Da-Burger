65dd363956f360ddee7e4cbd023a787f698d0260
Ankush <ankushchalla@gmail.com>
2021-02-16 13:11:47 -0800

    heroku troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log("connection details:", connection);
 }
 else {
   connection = mysql.createConnection({
